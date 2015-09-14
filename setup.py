c5b9623c0fe62fe5b65fe561224bb8ace058547a
Greg Fitch <greg.fitch@rice.edu>
2015-09-14 13:12:08 -0500

    project-ize me

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='PastaSauce',
+    version='0.1.1',
+    packages=['pastasauce', ],
+    license='Creative Commons Attribution-NonCommercial-ShareAlike 4.0 ' +
+            'International',
+    long_description=open('README.md').read(),
+)
